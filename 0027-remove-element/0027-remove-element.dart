59bcf77ee217b215e7967d83a5ad7432ed3fe24e
Gokul Krishnah <gokul.krishnahkv@gmail.com>
2025-09-01 14:28:07 +0530

    Time: 0 ms (100%), Space: 148 MB (77.14%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int removeElement(List<int> nums, int val) {
+    int j =0;
+    for (int i=0; i<nums.length ;i++){
+        if (nums[i] != val){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+  return j; }
+}
