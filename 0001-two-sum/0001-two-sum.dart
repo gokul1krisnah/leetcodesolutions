f028a08209b947636cdca4770c05d4219f2bc288
Gokul Krishnah <gokul.krishnahkv@gmail.com>
2025-12-16 15:49:21 +0530

    Time: 6 ms (76.6%), Space: 150.8 MB (31.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.dart
+++ 0001-two-sum/0001-two-sum.dart
@@ -1,12 +1,21 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    for (int i=0; i < nums.length; i++){
-        for (int j=i+1 ; j < nums.length; j++){
-            if (nums[i] + nums[j] == target){
-                return [i,j];
-            }
-        }
+    // Map to store number -> index
+    Map<int, int> map = {};
+
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+
+      // Check if complement already exists
+      if (map.containsKey(complement)) {
+        return [map[complement]!, i];
+      }
+
+      // Store current number with its index
+      map[nums[i]] = i;
     }
+
+    // As per LeetCode, there will always be a solution
     return [];
   }
 }
